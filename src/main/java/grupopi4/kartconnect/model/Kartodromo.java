2b96258ce8d2944101eaee21992ef7d570982de6
PedroOzassa <pedroozassa01@gmail.com>
2024-10-15 15:15:58 -0300

    Atualizacão de Kartodromo.java

--- src/main/java/grupopi4/kartconnect/model/Kartodromo.java
+++ src/main/java/grupopi4/kartconnect/model/Kartodromo.java
@@ -12,6 +12,12 @@ public class Kartodromo {
     private String nome;
     @BsonProperty("telefone")
     private String telefone;
+    @BsonProperty("whatsapp")
+    private String whatsapp;
+    @BsonProperty("desc")
+    private String desc;
+    @BsonProperty("horario_funcionamento")
+    private String horario_funcionamento;
 
     public String getNome() {
         return nome;
@@ -45,11 +51,38 @@ public class Kartodromo {
         this.email = email;
     }
 
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getHorario_funcionamento() {
+        return horario_funcionamento;
+    }
+
+    public String getWhatsapp() {
+        return whatsapp;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setHorario_funcionamento(String horario_funcionamento) {
+        this.horario_funcionamento = horario_funcionamento;
+    }
+
+    public void setWhatsapp(String whatsapp) {
+        this.whatsapp = whatsapp;
+    }
+
     @Override
     public String toString() {
         return "Nome: " + nome + "\n" +
                 "Endereço: " + endereco + "\n" +
                 "Telefone: " + telefone + "\n" +
-                "Email: " + email;
+                "Email: " + email + "\n" +
+                "Descrição: " + desc + "\n" +
+                "Whatsapp: " + whatsapp + "\n" +
+                "Horário de funcionamento: " + horario_funcionamento;
     }
 }
