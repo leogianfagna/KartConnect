359b6779784bb450eea02e36f0682cc756a57f5e
Léo <leogianfagna@gmail.com>
2024-10-15 11:08:13 -0300

    Portal de resultados filtrados por kartodromos e categorias

--- src/main/web/js/portal-filters-selector.js
+++ src/main/web/js/portal-filters-selector.js
@@ -1,16 +1,103 @@
+// Variáveis globais para identificar os filtros de resultados
+let selectedKartodromo = '';
+let selectedCategoria = 'all';
+
+
+// Gettters e setters para resgatar os filtros aplicados pelo usuário na página
+function setKartodromo(element) {
+    const kartodromoEscolhido = element.id;
+    selectedKartodromo = kartodromoEscolhido.replace('card-kart-', '');
+}
+
+function setCategoria(element) {
+    const categoria = (element.id).replace('cat-', '');
+    selectedCategoria = categoria;
+}
+
+function getKartodromo() {
+    return selectedKartodromo;
+}
+
+function getCategoria() {
+    return selectedCategoria;
+}
+
+
+// Executado toda vez ao clicar em uma categoria, vai adicionar os estilos necessários e buscar resultados baseado nos filtros
 function filtrarCategoria(element) {
+    setCategoria(element);
     limparSelecaoCategoria();
+    buscarClassificacoes();
+
     element.classList.add('option-button-selected');
 }
 
+
+// Executado toda vez ao clicar em um kartodromo, vai adicionar os estilos necessários e buscar resultados baseado nos filtros
 function filtrarKartodromo(element) {
+    setKartodromo(element);
     limparSelecaoKartodromo();
     diminuirOpacidade(element);
-    buscarClassificacoes(element.id);
+    buscarClassificacoes();
 
     element.classList.add('option-button-selected');
 }
 
+// Função para resgatar os valores do banco de dados, já com os filtros aplicados na página pelo usuário
+function buscarClassificacoes() {
+    console.log(`Kartodromo selecionado: ${selectedKartodromo}\nCategoria selecionada: ${selectedCategoria}`);
+
+    fetch('http://localhost:8080/api/classificacoes', { method: "GET" })
+        .then(response => response.json())
+        .then(classificacoes => {
+            showPortal();
+            const tbody = document.getElementById('tempos-banco');
+            limparTabelaBody(tbody);
+            classificacoes.forEach((classificacao, index) => {
+
+                // Cria uma nova linha na tabela
+                const tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <th scope="row">${index + 1}</th>
+                    <td>${classificacao.nome}</td>
+                    <td>${classificacao.estado}</td>
+                    <td>${classificacao.peso}</td>
+                    <td>${classificacao.tempo}</td>
+                `;
+
+                // Filtragem de resultados baseado nos filtros do usuário
+                if (classificacao.kartodromo === selectedKartodromo) {
+                    if (selectedCategoria === 'all') {
+                        tbody.appendChild(tr);
+                    } else if (pertenceCategoria(classificacao.peso) === true) {
+                        tbody.appendChild(tr);
+                    }
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar classificações:', error);
+        });
+}
+
+function pertenceCategoria(kg) {
+    if (selectedCategoria === 'all') return true; // Todos pertencem
+
+    const pesoPiloto = parseInt(kg);
+    let pesoMinimoPiloto, pesoMaximoPiloto;
+
+    (selectedCategoria === '60' ? pesoMinimoPiloto = 0 : pesoMinimoPiloto = parseInt(selectedCategoria));
+    (selectedCategoria === '100' ? pesoMaximoPiloto = 300 : pesoMaximoPiloto = parseInt(selectedCategoria) + 10);
+
+    if (pesoPiloto >= pesoMinimoPiloto && pesoPiloto < pesoMaximoPiloto) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// Funções auxiliares para alterar manipulação visual da página e criar imersão
 function limparSelecaoCategoria() {
     const div = document.getElementById('categorias');
     const elementos = div.querySelectorAll('.option-button-selected');
@@ -49,44 +136,11 @@ function voltarOpacidade() {
     }
 }
 
-function buscarClassificacoes(kartodromoFiltrado) {
-    kartodromoFiltrado = kartodromoFiltrado.replace('card-kart-', '');
-
-    fetch('http://localhost:8080/api/classificacoes', { method: "GET" })
-        .then(response => response.json())
-        .then(classificacoes => {
-            toggleContent();
-            const tbody = document.getElementById('tempos-banco');
-            limparTabelaBody(tbody);
-            classificacoes.forEach((classificacao, index) => {
-
-                // Cria uma nova linha na tabela
-                const tr = document.createElement('tr');
-                tr.innerHTML = `
-                    <th scope="row">${index + 1}</th>
-                    <td>${classificacao.nome}</td>
-                    <td>${classificacao.estado}</td>
-                    <td>${classificacao.peso}</td>
-                    <td>${classificacao.tempo}</td>
-                `;
-
-                if (classificacao.kartodromo == kartodromoFiltrado) {
-                    tbody.appendChild(tr); // Adicionar linha
-                }
-
-            });
-            console.log("classificações carregadas:", classificacoes);
-        })
-        .catch(error => {
-            console.error('Erro ao buscar classificações:', error);
-        });
-}
-
 function limparTabelaBody(tbody) {
     tbody.innerHTML = '';
 }
 
-function toggleContent() {
+function showPortal() {
     const content = document.getElementById('table-portal');
 
     // Confere se já não está exibido para não exibir novamente ou esconder
