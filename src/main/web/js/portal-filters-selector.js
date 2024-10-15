aa3d33c0493f6bf44972dd3e97c66c94d042b228
Léo <leogianfagna@gmail.com>
2024-10-15 00:58:46 -0300

    Tabela de resultados sincronizada com dados do banco

--- src/main/web/js/portal-filters-selector.js
+++ src/main/web/js/portal-filters-selector.js
@@ -6,6 +6,8 @@ function filtrarCategoria(element) {
 function filtrarKartodromo(element) {
     limparSelecaoKartodromo();
     diminuirOpacidade(element);
+    buscarClassificacoes(element.id);
+
     element.classList.add('option-button-selected');
 }
 
@@ -45,4 +47,40 @@ function voltarOpacidade() {
     for (let i = 0; i < elementos.length; i++) {
         elementos[i].style.opacity = 1;
     }
+}
+
+function buscarClassificacoes(kartodromoFiltrado) {
+    kartodromoFiltrado = kartodromoFiltrado.replace('card-kart-', '');
+
+    fetch('http://localhost:8080/api/classificacoes', { method: "GET" })
+        .then(response => response.json())
+        .then(classificacoes => {
+            const tbody = document.getElementById('tempos-banco');
+            limparTabelaBody(tbody);
+            classificacoes.forEach((classificacao, index) => {
+                
+                // Cria uma nova linha na tabela
+                const tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <th scope="row">${index + 1}</th>
+                    <td>${classificacao.nome}</td>
+                    <td>${classificacao.estado}</td>
+                    <td>${classificacao.peso}</td>
+                    <td>${classificacao.tempo}</td>
+                `;
+                
+                if (classificacao.kartodromo == kartodromoFiltrado) {
+                    tbody.appendChild(tr); // Adicionar linha
+                }
+                
+            });
+            console.log("classificações carregadas:", classificacoes);
+        })
+        .catch(error => {
+            console.error('Erro ao buscar classificações:', error);
+        });
+}
+
+function limparTabelaBody(tbody) {
+    tbody.innerHTML = '';
 }
