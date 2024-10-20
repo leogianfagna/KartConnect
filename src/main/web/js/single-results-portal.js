4a371cab7e200c56f285804a0ae62f0a64166afb
Léo <leogianfagna@gmail.com>
2024-10-20 15:18:12 -0300

    Busca por resultados individuais

--- src/main/web/js/single-results-portal.js
+++ src/main/web/js/single-results-portal.js
@@ -1,28 +1,161 @@
+const tbody = document.getElementById('tempos-banco');
+
+let filter = { "nome": 0 , "kartodromo": "" };
+let kartFilter = undefined;
+
+function getFiltros() {
+    const name = document.getElementById('pilot-name').value;
+
+    if (kartFilter === undefined) {
+        delete filter.kartodromo;
+    } else {
+        filter.kartodromo = kartFilter;
+    }
+    
+    filter.nome = name;
+
+    const queryParams = new URLSearchParams(filter).toString();
+    return queryParams;
+}
+
 // Função para resgatar os valores do banco de dados, já com os filtros aplicados na página pelo usuário
-let dados;
+function buscarClassificacoes() {
+    let tdId = 1;
+    const filtro = getFiltros();
+    limparTabelaBody();
+    console.log("Filtro aplicado = " + filtro);
 
+    fetch(`http://localhost:8080/api/classificacoes?${filtro}`, { method: "GET" })
+        .then(response => response.json())
+        .then(classificacoes => {
+            showPortal();
 
-async function buscarClassificacoes() {
-    try {
-        const response = await fetch('http://localhost:8080/api/classificacoes', { method: "GET" });
-        const classificacoes = await response.json();
-        const arrayFiltrado = classificacoes.filter(obj => obj.nome === "Gustavo Andrade");
+            classificacoes.forEach((classificacao, index) => {
 
-        console.log(classificacoes);
-        console.log(arrayFiltrado);
+                // Cria uma nova linha na tabela
+                const tr = document.createElement('tr');
+                tr.innerHTML = `
+                    <td>${classificacao.nome}</td>
+                    <td>${classificacao.peso}</td>
+                    <td>${classificacao.kartodromo}</td>
+                    <td>${classificacao.tempo}</td>
+                    <td>ANALISAR</td>
+                `;
 
-        return arrayFiltrado;
-    } catch (error) {
-        console.error('Erro ao buscar classificações:', error);
+                tdId++;
+                tbody.appendChild(tr);
+            });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar classificações:', error);
+        });
+}
+
+// Seleção de opções
+// Executado toda vez ao clicar em uma categoria, vai adicionar os estilos necessários e buscar resultados baseado nos filtros
+function filtrarCategoria(element) {
+    setKartodromo(element);
+    limparSelecaoCategoria();
+    buscarClassificacoes();
+
+    element.classList.add('option-button-selected');
+}
+
+function setKartodromo(element) {
+    const kartodromoSelecionado = (element.id).replace('kart-', '');
+    if (kartodromoSelecionado === 'all') {
+        kartFilter = undefined;
+    } else {
+        kartFilter = kartodromoSelecionado;
     }
 }
 
-async function searchResults() {
-    const a = document.getElementById('pilot-name').value;
-    const dados = await buscarClassificacoes();
-    console.log("-> ", dados[0]);
+// Funções auxiliares para alterar manipulação visual da página e criar imersão
+function limparSelecaoCategoria() {
+    const div = document.getElementById('kartodromos-filtro');
+    const elementos = div.querySelectorAll('.option-button-selected');
+    elementos.forEach(function (elemento) {
+        elemento.classList.remove('option-button-selected');
+    });
 }
 
-searchResults();
+function limparTabelaBody() {
+    tbody.innerHTML = '';
+}
+
+function showPortal() {
+    const content = document.getElementById('table-portal');
+
+    // Confere se já não está exibido para não exibir novamente ou esconder
+    if (!content.classList.contains('show')) {
+        content.style.display = 'block';
+        content.classList.add('show');
+    }
+}
+
+// Velocímetro
+const speedometerNeedle = document.querySelector('.speedometer-needle');
+const statusText = document.getElementById("status-txt");
+const background = document.getElementById("speedometer-background");
+
+function setSpeedometerValue(degrees) {
+    if (degrees == -72) {
+        speedometerNeedle.style.transform = `rotate(${degrees}deg) translateX(-50%)`;
+        statusText.innerHTML = "Strong Sell";
+        statusText.style.color = "#f44336";
+        background.style.backgroundImage = "radial-gradient(#f4433600,#f443361c, #f443365b)";
 
 
+    } else if (degrees == -36) {
+        speedometerNeedle.style.transform = `rotate(${degrees}deg) translateX(-50%)`;
+        statusText.innerHTML = "Sell";
+        statusText.style.color = "#d33a2f";
+        background.style.backgroundImage = "radial-gradient(#d33a2f00,#d33a2f0c, #d33a2f5b)";
+
+
+    } else if (degrees == 0) {
+        speedometerNeedle.style.transform = `rotate(${degrees}deg) translateX(-50%)`;
+        statusText.innerHTML = "Neutral";
+        statusText.style.color = "#787B86";
+        background.style.backgroundImage = "radial-gradient(#787B8600,#787B860c, #787B865b)";
+
+
+    } else if (degrees == 36) {
+        speedometerNeedle.style.transform = `rotate(${degrees}deg) translateX(-50%)`;
+        statusText.innerHTML = "Buy";
+        statusText.style.color = "#2457e6";
+        background.style.backgroundImage = "radial-gradient(#2457e600,#2457e60c, #2457e65b)";
+
+
+    } else if (degrees == 72) {
+        speedometerNeedle.style.transform = `rotate(${degrees}deg) translateX(-50%)`;
+        statusText.innerHTML = "Strong Buy";
+        statusText.style.color = "#2962ff";
+        background.style.backgroundImage = "radial-gradient(#2962ff00,#2962ff1c, #2962ff5b)";
+
+    } else {
+        alert("Invalid Value");
+    }
+    speedometerNeedle.style.transform = `rotate(${degrees}deg) translateX(-50%)`;
+}
+
+function setScore(score) {
+    var degrees;
+    if (score == 0) {
+        degrees = 0;
+    } else if (score == 1) {
+        degrees = 36;
+    } else if (score >= 2) {
+        degrees = 72;
+    } else if (score == -1) {
+        degrees = -36;
+    } else if (score <= -2) {
+        degrees = -72;
+    } else {
+        alert("Invalid Score");
+    }
+    setSpeedometerValue(degrees);
+}
+
+
+setScore(1);//Input Score
