8bcb1eb518c30979bbebce2317a8d13c30c250ef
Léo <leogianfagna@gmail.com>
2024-10-20 17:29:32 -0300

    Comparação de estatísticas com dashboard

--- src/main/web/js/single-results-portal.js
+++ src/main/web/js/single-results-portal.js
@@ -1,6 +1,6 @@
 const tbody = document.getElementById('tempos-banco');
 
-let filter = { "nome": 0 , "kartodromo": "" };
+let filter = { "nome": 0, "kartodromo": "" };
 let kartFilter = undefined;
 
 function getFiltros() {
@@ -11,7 +11,7 @@ function getFiltros() {
     } else {
         filter.kartodromo = kartFilter;
     }
-    
+
     filter.nome = name;
 
     const queryParams = new URLSearchParams(filter).toString();
@@ -30,7 +30,7 @@ function buscarClassificacoes() {
         .then(classificacoes => {
             showPortal();
 
-            classificacoes.forEach((classificacao, index) => {
+            classificacoes.forEach((classificacao) => {
 
                 // Cria uma nova linha na tabela
                 const tr = document.createElement('tr');
@@ -39,8 +39,23 @@ function buscarClassificacoes() {
                     <td>${classificacao.peso}</td>
                     <td>${classificacao.kartodromo}</td>
                     <td>${classificacao.tempo}</td>
-                    <td>ANALISAR</td>
                 `;
+
+                // Cria o botão separadamente
+                const td = document.createElement('td');
+                const btn = document.createElement('p'); // Usando 'p' como botão
+                btn.className = "mb-3 query-simple-button";
+                btn.style.fontSize = "1em";
+                btn.textContent = "ANALISAR";
+
+                // Adiciona o evento 'onclick' corretamente
+                btn.addEventListener('click', function () {
+                    showDashboard(classificacao.kartodromo);
+                });
+
+                // Adiciona o botão na célula e a célula na linha
+                td.appendChild(btn);
+                tr.appendChild(td);
 
                 tdId++;
                 tbody.appendChild(tr);
@@ -50,6 +65,140 @@ function buscarClassificacoes() {
             console.error('Erro ao buscar classificações:', error);
         });
 }
+
+
+async function showDashboard(kartodromoAnalisado) {
+    const newFilter = { "kartodromo": kartodromoAnalisado };
+    const queryParams = new URLSearchParams(newFilter).toString();
+
+    let tempoPiloto;
+    let pos1Tempo;
+    let pos10Tempo;
+    let mediaTempoTotal = 0;
+    let mediaTempoParcial = 0;
+    let mediaTempo;
+    let melhorTempo;
+    let piorTempo;
+    let meioMelhorMedia;
+    let meioPiorMedia;
+    let i = 0;
+
+    try {
+        // Vai buscar todos os resultados relacionado ao kartódromo em questão
+        const response = await fetch(`http://localhost:8080/api/classificacoes?${queryParams}`, { method: "GET" });
+        const classificacoes = await response.json();
+
+        pos10Tempo = classificacoes[9].tempo;
+        pos1Tempo = classificacoes[0].tempo;
+
+        // Iterar apenas os 20 primeiros tempos para informações no dashboard
+        const top20Classificacoes = classificacoes.slice(0, 20);
+        top20Classificacoes.forEach((classificacao) => {
+            mediaTempoParcial += timeToMilliseconds(classificacao.tempo);
+        });
+
+        mediaTempo = mediaTempoParcial / top20Classificacoes.length;
+        melhorTempo = timeToMilliseconds(top20Classificacoes[0].tempo);
+        piorTempo = timeToMilliseconds(top20Classificacoes[top20Classificacoes.length - 1].tempo);
+        meioMelhorMedia = (melhorTempo + mediaTempo) / 2;
+        meioPiorMedia = (piorTempo + mediaTempo) / 2;
+
+        // Processa os demais tempos para conseguir a média total
+        classificacoes.forEach((classificacao) => {
+            if (classificacao.nome === document.getElementById('pilot-name').value) {
+                tempoPiloto = classificacao.tempo;
+                console.log("Tempo do piloto encontrado = " + tempoPiloto);
+            }
+
+            i++;
+            mediaTempoTotal += timeToMilliseconds(classificacao.tempo);
+        });
+
+    } catch (error) {
+        // Captura erros no fetch ou no processamento
+        console.error('Erro ao buscar classificações:', error);
+        // TO-DO: FAZER UM ERRO NO SITE
+        return;
+    }
+
+    mediaTempoTotal = millisecondsToTime((mediaTempoTotal / i))
+    document.getElementById('dif-first').innerHTML = getDifTime(tempoPiloto, pos1Tempo);
+    document.getElementById('dif-top10').innerHTML = getDifTime(tempoPiloto, pos10Tempo);
+    document.getElementById('dif-media').innerHTML = getDifTime(tempoPiloto, mediaTempoTotal);
+
+    definirVelocimetro(melhorTempo, piorTempo, mediaTempo, meioMelhorMedia, meioPiorMedia, tempoPiloto);
+    console.log("Melhor tempo = " + millisecondsToTime(melhorTempo));
+    console.log("Pior tempo = " + millisecondsToTime(piorTempo));
+    console.log("Média dos 20 tempos = " + millisecondsToTime(mediaTempo));
+    console.log("Meio entre o melhor tempo e a média = " + millisecondsToTime(meioMelhorMedia));
+    console.log("Meio entre o pior tempo e a média = " + millisecondsToTime(meioPiorMedia));
+
+    // Confere se já não está exibido para não exibir novamente ou esconder
+    const content = document.getElementById('dashboard-portal');
+    if (!content.classList.contains('show')) {
+        content.style.display = 'block';
+        content.classList.add('show');
+    }
+}
+
+// to-do: Fix
+function definirVelocimetro(melhorTempo, piorTempo, mediaTempo, meioMelhorMedia, meioPiorMedia, tempoPiloto) {
+    switch (true) {
+        case (millisecondsToTime(tempoPiloto) < millisecondsToTime(meioMelhorMedia)):
+            setScore(2);
+            break;
+        case (millisecondsToTime(tempoPiloto) < millisecondsToTime(mediaTempo)):
+            setScore(1);
+            break;
+        case (millisecondsToTime(tempoPiloto) < millisecondsToTime(meioPiorMedia)):
+            setScore(0);
+            break;
+        case (millisecondsToTime(tempoPiloto) < millisecondsToTime(piorTempo)):
+            setScore(-1);
+            break;
+        default:
+            setScore(-2);
+            break;
+    }
+}
+
+function getDifTime(tempo1, tempo2) {
+    tempo1 = timeToMilliseconds(tempo1);
+    tempo2 = timeToMilliseconds(tempo2);
+
+    const difference = tempo1 - tempo2;
+
+    // Certifica-se de que estamos lidando com o valor absoluto da diferença e armazenando o sinal
+    const isNegative = difference < 0;
+    const resultTime = millisecondsToTime(Math.abs(difference));
+
+    // Adiciona o sinal negativo ao resultado se for necessário
+    return isNegative ? `-${resultTime}` : resultTime;
+}
+
+function timeToMilliseconds(timeString) {
+    const [minutes, seconds, milliseconds] = timeString.split(':').map(Number);
+
+    // Converte minutos, segundos e milissegundos para milissegundos totais
+    const totalMilliseconds = (minutes * 60 * 1000) + (seconds * 1000) + milliseconds;
+
+    return totalMilliseconds;
+}
+
+function millisecondsToTime(milliseconds) {
+    const minutes = Math.floor(milliseconds / (60 * 1000));
+    milliseconds %= (60 * 1000);
+
+    const seconds = Math.floor(milliseconds / 1000);
+    const remainingMilliseconds = Math.abs(Math.round(milliseconds % 1000)); // Arredonda milissegundos e garante que seja positivo
+
+    // Formata o tempo para que sempre tenha dois dígitos em segundos e três em milissegundos
+    return `${minutes}:${seconds.toString().padStart(2, '0')}:${remainingMilliseconds.toString().padStart(3, '0')}`;
+}
+
+
+
+
 
 // Seleção de opções
 // Executado toda vez ao clicar em uma categoria, vai adicionar os estilos necessários e buscar resultados baseado nos filtros
@@ -156,6 +305,3 @@ function setScore(score) {
     }
     setSpeedometerValue(degrees);
 }
-
-
-setScore(1);//Input Score
