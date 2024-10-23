e6627535736fe4fb075ccef374e0962c14f2d905
PedroOzassa <pedroozassa01@gmail.com>
2024-10-23 16:33:35 -0300

    Gerador de classificaões e atualização de classes

--- src/main/java/grupopi4/kartconnect/Main.java
+++ src/main/java/grupopi4/kartconnect/Main.java
@@ -11,22 +11,10 @@ import java.net.InetSocketAddress;
 public class Main {
     public static void main(String[] args) {
         try {
-            int port = 8080;
+            GeradorClassificacao.gerarClassificacoes(-100);
 
-
-            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-
-            server.createContext("/api/kartodromos", new KartodromoHandler());
-            server.createContext("/api/classificacoes", new ClassificacaoHandler());
-            server.createContext("/api/participantes", new PilotoHandler());
-
-            server.setExecutor(null);
-
-            System.out.println("Servidor iniciado na porta " + port);
-            server.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("Falha ao iniciar o servidor.");
+        } catch (Exception erro) {
+            erro.printStackTrace();
         }
     }
 }
